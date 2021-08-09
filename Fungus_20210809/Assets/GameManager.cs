b3dc512112cd7aa020f548b577db439b5a3842c0
KID <50666207+KID421@users.noreply.github.com>
2021-08-09 21:26:27 +0800

    Fungus 對話、腳本溝通方式

---
+++ Fungus_20210809/Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Fungus;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("流程圖物件")]
+    public Flowchart flowchartObject;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            flowchartObject.SendFungusMessage("按下空白鍵");
+        }
+    }
+}
